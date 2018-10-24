6f84908ba3e54cfd915c74d9c36707d05260ae8e
Balletti-Laurent <balletti.laurent@gmail.com>
2018-10-24 10:38:11 +0200

    Exercice 7

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,14 @@
+function show(){
+  var firstNumber= document.getElementById('shoeSize').value;
+  var secondNumber= document.getElementById('yearOfBirth').value;
+  var shoeSize2= (shoeSize)*2;
+  var shoeSize5= (shoeSize2)+5;
+  var 
+  alert(result);
+  if(isNaN(result)==true){
+  alert('Erreur, ceci n\'est pas un nombre');
+}
+else {
+  alert('Voici votre résultat '+ result);
+}
+}
